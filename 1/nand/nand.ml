33015e7405f4fba68241a061adf296568a92ce26
gguerrini2 <g.guerrini2@studenti.unica.it>
2024-10-13 14:19:58 +0200

    nand

--- 1/nand/nand.ml
+++ 1/nand/nand.ml
@@ -1,7 +1,1 @@
-(* This is an OCaml editor.
-   Enter your program here and send it to the toplevel using the "Eval code"
-   button or [Ctrl-e]. *)
-
-let nand a b = not (a && b);; 
-
-  
+let nand a b = not (a && b);;
