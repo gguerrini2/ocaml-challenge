6f1f20e07402a3c0a81d8d85084b093a4d6a40b9
g.guerrini2@studenti.unica.it <g.guerrini2@studenti.unica.it>
2024-10-18 11:41:23 +0200

    agg

--- 1/nand/nand.ml
+++ 1/nand/nand.ml
@@ -1,1 +1,15 @@
+let a = true;;
+
+let b = false;;
+
 let nand a b = not (a && b);;
+
+let nand2 (a : bool) (b : bool) :
+bool = if a
+  then (*[a] is true*)
+    (if b
+      then (*[b] is true*) false
+  else true)
+
+else(*[a] is false*)
+false
