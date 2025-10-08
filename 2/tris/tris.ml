ab6fc4640686cb13bfc1d7bf726a9e1daa0b72e4
g.guerrini2@studenti.unica.it <g.guerrini2@studenti.unica.it>
2025-10-08 17:34:51 +0200

    tris

---
+++ 2/tris/tris.ml
@@ -0,0 +1,22 @@
+(*# Tris
+
+Write a function with type:
+```ocaml
+tris : 'a * 'a * 'a * 'a -> bool
+```
+such that `tris (a,b,c,d)` is true iff at least three elements of the quadruple `(a,b,c,d)` are equal.
+
+Then, test the function through a function with type:
+```ocaml
+hand : unit -> int * int * int * int
+```
+which generates a random quadruple of elements in the range 1..10.*)
+
+Random.self_init();;
+
+let tris (a, b, c, d: 'a * 'a * 'a * 'a) = match (a, b, c, d) with
+|(a, b, c, d) when (a = b && b = c && c = d) || (a = b && b = c ) || (b = c && c = d) || (a = c && c = d) || (a = b && b = d) -> true
+|_ -> false;;
+
+let hand () = (((Random.int 10) mod 10) + 1), (((Random.int 10) mod 10) + 1), (((Random.int 10) mod 10) + 1), (((Random.int 10) mod 10) + 1);
+
