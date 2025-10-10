99b5b86f9558a746502c66eac1123c4172da4c3b
g.guerrini2@studenti.unica.it <g.guerrini2@studenti.unica.it>
2025-10-10 18:10:09 +0200

    compose e nuovo readme

--- 2/compose/compose.ml
+++ 2/compose/compose.ml
@@ -23,4 +23,13 @@ assert(comp (fun b -> if b then 0 else 1) (fun x -> x>0) 0 = 1);;
 ```
 *)
 
-let comp f g = f (g());;
+let comp (f: 'b -> 'c) (g: 'a -> 'b) = fun x -> f (g x);;
+
+let double x = x * 2;;
+let square x = x * x;;
+
+assert((comp square double) 3 = 36);;
+assert((comp double square) 3 = 18);;
+
+assert(comp (fun b -> if b then 0 else 1) (fun x -> x>0) 3 = 0);;
+assert(comp (fun b -> if b then 0 else 1) (fun x -> x>0) 0 = 1);;
