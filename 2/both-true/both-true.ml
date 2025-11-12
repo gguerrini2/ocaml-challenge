aca3ace858333fe4b13428ee05f0cbe191f59f38
Giuseppe <g.guerrini2@studenti.unica.it>
2025-11-12 12:11:55 +0100

    Add OCaml function `both_true` and related unit tests; create initial settings and README files

---
+++ 2/both-true/both-true.ml
@@ -0,0 +1,26 @@
+(*# Both true
+
+Write a function `both_true` that takes as input two predicates `p` and `q`, each of type `'a -> bool`, and returns a new function that checks 
+if both `p` and `q` are true for a given argument.
+
+The function must have the following type:
+```ocaml
+both_true : ('a -> bool) -> ('a -> bool) -> 'a -> bool
+```
+
+Here are some unit tests:
+```ocaml
+let is_positive x = x > 0;;
+let is_even x = x mod 2 = 0;;
+let is_positive_and_even = both_true is_positive is_even;;
+assert(is_positive_and_even 4 = true);;
+assert(is_positive_and_even 3 = false);;
+```*)
+
+let both_true a b x = if(a x) && (b x) then true else false;;
+
+let is_positive x = x > 0;;
+let is_even x = x mod 2 = 0;;
+let is_positive_and_even = both_true is_positive is_even;;
+assert(is_positive_and_even 4 = true);;
+assert(is_positive_and_even 3 = false);;
